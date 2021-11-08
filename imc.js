e33d9d10b660d76c5b89070ab43d887abd39f09d
anamsilva1981 <ana.nikoly@gmail.com>
2021-11-08 20:04:50 -0300

    calculadora-finalizada

---
+++ imc.js
@@ -0,0 +1,38 @@
+
+const calcular = document.getElementById('calcular');
+
+function imc () {
+
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== ''){
+
+        const valorIMC = (peso / (altura* altura)).toFixed(0);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.50){
+            classificacao = 'Abaixo do peso.';
+        } else if (valorIMC < 25) {
+            classificacao = 'com peso ideal';
+        } else if (valorIMC < 30) {
+            classificacao = 'levemente acima do peso';
+        } else if (valorIMC < 35) {
+            classificacao = 'obesidade grau I';
+        }else if (valorIMC < 40) {
+            classificacao = 'Obesidade grau II';
+        }else {
+            classificacao = 'com obesidade grau III, cuidado!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+    }else{
+        resultado.textContent = 'Preencha todos os campos'   
+    }
+}
+
+// Escuta quando um evento acontece
+calcular.addEventListener('click', imc);
